20be5fdbc0e742ca28a5e72fe4e78e1c507208f6
Magdalena <magdalenamalek52@gmail.comgit statusgit status git config --global user.email magdalenamalek52@gmail.com>
2023-04-17 10:00:34 +0200

    prepare project for publication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require ('dotenv/config');
 const affirmationsRoute = require('./routes/affirmations.routes');
 
@@ -15,17 +16,24 @@ if (process.env.NODE_ENV !== 'production') {
     );
 };
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-app.use('/api', affirmationsRoute);
-
 mongoose.
 connect(process.env.DB_CONNECTION)
 .then(() => {
     console.log('connected to datebase')
-    app.listen(9000, () => {
+    app.listen(process.env.PORT || 9000, () => {
         console.log("app is running on port 9000")
     });
 }).catch((error) => {
     console.log(error)
 });
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use('/api', affirmationsRoute);
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });  
+
